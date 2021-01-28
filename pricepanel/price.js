6fa9463a5b06233183f67c61baa1e0526c972209
codetobuild <goodnakha@gmail.com>
2021-01-28 23:47:00 +0530

    pricepanel added javascript code

---
+++ pricepanel/price.js
@@ -0,0 +1,40 @@
+
+const price = document.querySelector('.price');
+
+const slider= document.querySelector('#myrange');
+price.innerHTML=`$ ${slider.value}.00`
+
+slider.addEventListener('input' , ()=>{
+    console.log(slider.value);
+    price.innerHTML=`$ ${slider.value}.00`;
+})
+
+
+const btn= document.querySelector('.circle')
+
+btn.addEventListener('click', (e)=>{
+ 
+    const parent =   e.target.parentNode;
+   
+      btn.classList.toggle('circle-on');
+
+     if(  btn.classList.contains('circle-on'))
+     {
+      parent.style.backgroundColor="dodgerblue";
+     }
+     else{
+        parent.style.backgroundColor="rgb(201, 201, 201)";
+     }
+})
+
+
+
+
+
+
+
+
+
+
+
+
